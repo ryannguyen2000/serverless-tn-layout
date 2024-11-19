1beea1bdb269d53b3f601f9257faa68f736c7937
datnt19213 <datnguyen.frontend.dev@gmail.com>
2024-11-19 14:35:20 +0700

    fix: api publish

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: [process.env.FE_HOST, process.env.FE_PORT2],
+    origin: process.env.FE_HOST || process.env.FE_PORT_2,
     methods: ["GET", "POST"],
     credentials: true,
   })
@@ -22,8 +22,8 @@ app.use(
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [process.env.FE_HOST, process.env.FE_HOST2],
-    methods: ["GET", "POST"],
+    origin: process.env.FE_HOST || process.env.FE_HOST_2,
+    methods: ["POST"],
     credentials: true,
   },
 });
@@ -50,6 +50,14 @@ app.post("/webhook", (req, res) => {
   console.log("Webhook received:", req.body);
 
   io.emit("webhook-data", req.body);
+
+  res.status(200).send("Webhook received and broadcasted");
+});
+
+app.post("/publish", (req, res) => {
+  console.log("Webhook received:", req.body);
+
+  io.emit("return-json", req.body);
 
   res.status(200).send("Webhook received and broadcasted");
 });
